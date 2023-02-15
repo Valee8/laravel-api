0072f3e3624da1503a3f33cd61b6b7af1b401e3b
Vale <oirelav95@gmail.com>
2023-02-15 19:32:23 +0100

    creo tabelle movies, genres e tags

--- database/migrations/2023_02_15_143929_create_movies_table.php
+++ database/migrations/2023_02_15_143929_create_movies_table.php
@@ -16,7 +16,7 @@ return new class extends Migration
         Schema::create('movies', function (Blueprint $table) {
             $table -> id();
 
-            $table -> string('name', 64) -> unique();
+            $table -> string('name', 64);
             $table -> year('year');
             $table -> integer('cashOut') -> unsigned() -> nullable();
 
