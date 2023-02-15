b379e41381be401084162386b23c26d1ac0482c6
Vale <oirelav95@gmail.com>
2023-02-15 22:59:39 +0100

    creo file home per mostrare lista movies

---
+++ resources/views/pages/movie/home.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.main-layout')
+
+@section('content')
+
+    <h1>
+        Movies
+    </h1>
+
+    <h2>
+        <a href="{{ route('create.movie ')}}">Create new Movie</a>
+    </h2>
+
+    <a href="{{ route('home') }}">Back</a>
+
+    <ul>
+        @foreach ($movies as $movie)
+            <li>
+                Name: {{ $movie -> name }} - Year: {{ $movie -> year }} - Cash Out: {{ $movie -> cashOut }}&dollar;
+                - <a href="{{ route('movie.delete', $movie )}}">Delete</a>
+            </li>
+        @endforeach
+    </ul>
+    
+@endsection
