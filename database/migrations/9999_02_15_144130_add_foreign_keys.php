5e79e66cf49880c9a993c2a12abc4c13382a23f4
Vale <oirelav95@gmail.com>
2023-02-15 16:07:19 +0100

    creo tabella dove aggiungo le foreign keys

---
+++ database/migrations/9999_02_15_144130_add_foreign_keys.php
@@ -0,0 +1,53 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up(): void
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            
+            $table -> foreignId('genre_id')
+                   -> constrained();
+
+        });
+
+        Schema::table('tag_movie', function (Blueprint $table) {
+
+            $table -> foreignId('tag_id')
+                   -> constrained();
+
+            $table -> foreignId('movie_id')
+                   -> constrained();
+        
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('movies', function (Blueprint $table) {
+            
+            $table -> dropForeign('movies_genre_id_foreign');
+
+        });
+
+        Schema::table('tag_movie', function (Blueprint $table) {
+
+            $table -> dropForeign('tag_movie_tag_id_foreign');
+
+            $table -> dropForeign('tag_movie_movie_id_foreign');
+        
+        });
+    }
+};
