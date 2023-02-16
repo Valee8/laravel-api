8815c19f916ca7b0857c8e2cda49de55b7aeb30f
Vale <oirelav95@gmail.com>
2023-02-16 12:59:28 +0100

    aggiungo rotte per modificare movie

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/movie/delete/{movie}', [MainController::class, 'movieDelete']) -> n
 Route::get('/movie/create/', [MainController::class, 'movieCreate']) -> name('movie.create');
 
 Route::post('/movie/store/', [MainController::class, 'movieStore']) -> name('movie.store');
+
+Route::get('/movie/edit/{movie}', [MainController::class, 'movieEdit']) -> name('movie.edit');
+
+Route::post('/movie/update/{movie}', [MainController::class, 'movieUpdate']) -> name('movie.update');
