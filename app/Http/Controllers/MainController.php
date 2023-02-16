a0d7736751c032f49d1aa0999decbc3122cf6b1d
Vale <oirelav95@gmail.com>
2023-02-16 12:00:13 +0100

    aggiungo metodi per creare nuovo movie

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -25,10 +25,41 @@ class MainController extends Controller
     }
 
     public function movieDelete(Movie $movie) {
-        
+
         $movie -> tags() -> sync([]);
 
         $movie -> delete();
+
+        return redirect() -> route('movie.home');
+    }
+
+    public function movieCreate() {
+
+        $tags = Tag::all();
+
+        $genres = Genre::all();
+
+        return view('pages.movie.create', compact('tags', 'genres'));
+    }
+
+    public function movieStore(Request $request) {
+
+        $data = $request -> validate([
+            'name' => 'required|string|min:3|max:64',
+            'year' => 'required|integer|min:1900',
+            'cashOut' => 'nullable|integer|min:0',
+            'genre_id' => 'required|integer',
+            'tags_id' => 'required|array',
+        ]);
+
+        $movie = Movie::make($data);
+        $genre = Genre::find($data['genre_id']);
+
+        $movie -> genre() -> associate($genre);
+        $movie -> save();
+
+        $tags = Tag::find($data['tags_id']);
+        $movie -> tags() -> attach($tags);
 
         return redirect() -> route('movie.home');
     }
