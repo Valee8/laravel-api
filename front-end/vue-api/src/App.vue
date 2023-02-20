0aeeeb163f84eac2c46d8f7c78380559f55ca6f8
Vale <oirelav95@gmail.com>
2023-02-20 15:39:19 +0100

    aggiungo form per creare nuovi movies

--- front-end/vue-api/src/App.vue
+++ front-end/vue-api/src/App.vue
@@ -2,29 +2,102 @@
 
 import axios from 'axios';
 
+const api_url = 'http://localhost:8000/';
+
 export default {
 
+  data() {
+    return {
+      movies: [],
+      tags: [],
+      genres: [],
+      newMovie: {
+        tags_id: []
+      }
+    }
+  },
+  methods: {
+    updateData() {
+      axios.get(api_url + 'api/movie/v1/all')
+        .then(res => {
+
+          const data = res.data;
+          const success = data.success;
+          this.movies = data.response.movies;
+          this.tags = data.response.tags;
+          this.genres = data.response.genres;
+
+          console.log(this.genres);
+          console.log(this.movies);
+          console.log(this.tags);
+        })
+        .catch(err => console.error(err));
+    },
+    movieCreate(e) {
+      e.preventDefault();
+
+      const newMovie = this.newMovie;
+
+      axios.post(api_url + 'api/movie/v1/store', newMovie)
+        .then(res => {
+
+          const data = res.data;
+          const success = data.success;
+
+          this.updateData();
+
+          console.log(newMovie);
+        })
+        .catch(err => console.error(err));
+    }
+  },
   mounted() {
-
-    axios.get('http://127.0.0.1:8000/movie/v1/all')
-      .then(res => {
-
-        const data = res.data;
-        const success = data.success;
-        const movies = data.response;
-
-        //console.log(movies);
-      })
-      .catch(err => console.error(err));
+    this.updateData();
   }
 };
 
 </script>
 
 <template>
+  <form>
+    <label for="name">Name: </label>
+    <input type="text" name="name" v-model="newMovie.name">
+    <br>
+    <label for="year">Year: </label>
+    <input type="text" name="year" v-model="newMovie.year">
+    <br>
+    <label for="cashOut">Cash Out: </label>
+    <input type="number" name="cashOut" v-model="newMovie.cashOut">
+    <br>
+    <label name="genre">Genres: </label>
+    <select name="genre_id" v-model="newMovie.genre_id">
+      <option v-for="genre in genres" :value="genre.id" :key="genre.id">{{ genre.name }}</option>
+    </select>
+    <br>
+    <div v-for="tag in tags" :key="tag.id">
+      <input type="checkbox" :value="tag.id" v-model="newMovie.tags_id">
+      <label :for="tag.id">{{ tag.name }}</label>
+    </div>
+    <br>
+    <input type="submit" value="CREATE" @click="movieCreate">
+  </form>
+
   <h1>
-    Hello world
-</h1>
+    Movies
+  </h1>
+
+  <div v-for="(genre, index) in genres" :key="index">
+    <h2>
+      {{ genre.name }}
+    </h2>
+
+    <ul v-for="movie in movies">
+      <li v-if="genre.id === movie.genre_id">
+        {{ movie.name }}
+      </li>
+    </ul>
+
+  </div>
 </template>
 
 <style scoped>
