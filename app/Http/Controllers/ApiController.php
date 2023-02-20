d4de7d2884a33029894e8bf744447279af5a85fd
Vale <oirelav95@gmail.com>
2023-02-20 15:38:32 +0100

    modifico metodo per mostrare i movies

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -5,17 +5,25 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Models\Movie;
+use App\Models\Tag;
+use App\Models\Genre;
 
 
 class ApiController extends Controller
 {
     public function movieAll() {
 
+        $tags = Tag::all();
         $movies = Movie::all();
+        $genres = Genre::all();
 
         return response() ->json([
             'success' => true,
-            'responde' => $movies,
+            'response' => [
+                'movies' => $movies,
+                'genres' => $genres,
+                'tags' => $tags,
+            ]
         ]);
     }
 }
