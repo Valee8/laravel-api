c55442662e948ea0a0ec6c40e929b10a18577b4e
Vale <oirelav95@gmail.com>
2023-02-20 15:38:42 +0100

    aggiungo metodo movieStore

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -26,4 +26,30 @@ class ApiController extends Controller
             ]
         ]);
     }
+
+    public function movieStore(Request $request) {
+
+        $data = $request -> validate([
+            'name' => 'required|string|min:3|max:64',
+            'year' => 'required|integer|min:1900',
+            'cashOut' => 'nullable|integer|min:0',
+            'genre_id' => 'required|integer|min:1',
+            'tags_id' => 'nullable|array',
+        ]);
+
+        $movie = Movie::make($data);
+        $genre = Genre::find($data['genre_id']);
+
+        $movie -> genre() -> associate($genre);
+        $movie -> save();
+
+        $tags = Tag::find($data['tags_id']);
+        $movie -> tags() -> attach($tags);
+
+        return response() -> json([
+            'success' => true,
+            'response' => $movie,
+            'data' => $request -> all()
+        ]);
+    }
 }
