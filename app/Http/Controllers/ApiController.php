cb36085e8c061abb627aaa17d0396f5fd483191a
Vale <oirelav95@gmail.com>
2023-02-21 17:54:22 +0100

    aggiungo metodo per modificare movie

--- app/Http/Controllers/ApiController.php
+++ app/Http/Controllers/ApiController.php
@@ -14,7 +14,9 @@ class ApiController extends Controller
     public function movieAll() {
 
         $tags = Tag::all();
-        $movies = Movie::all();
+        $movies = Movie :: with('tags') 
+            -> orderBy('created_at', 'desc')
+            -> get();
         $genres = Genre::all();
 
         return response() ->json([
@@ -60,6 +62,34 @@ class ApiController extends Controller
 
         return response() -> json([
             'success' => true
+        ]);
+    }
+
+    public function movieUpdate(Request $request, Movie $movie) {
+
+        $data = $request -> validate([
+            'name' => 'required|string|min:3',
+            'year' => 'required|integer|min:0',
+            'cashOut' => 'required|integer|min:0',
+            'genre_id' => 'required|integer|min:1',
+            'tags_id' => 'nullable|array'
+        ]);
+
+        $genre = Genre :: find($data['genre_id']);
+        $movie -> update($data);
+        $movie -> genre() -> associate($genre);
+        $movie -> save();
+
+        if (array_key_exists('tags_id', $data)) {
+
+            $tags = Tag :: find($data['tags_id']);
+            $movie -> tags() -> sync($tags);
+        }
+    
+        return response() -> json([
+            'success' => true,
+            'response' => $movie,
+            'data' => $request -> all()
         ]);
     }
 }
